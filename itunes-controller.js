2af4729abec426cd1e97656b9abbb8818e285f38
Ramsey Bland <ramsey.bland@gmail.com>
2018-05-03 13:24:47 -0600

    bunch of styling

--- itunes-controller.js
+++ itunes-controller.js
@@ -5,7 +5,7 @@ function ItunesController() {
     e.preventDefault();
     var artist = e.target.artist.value;
     itunesService.getMusicByArtist(artist).then(draw);
-    document.getElementById("myForm").reset(); //after get music by artist returns what are you doing with the objects?
+    document.getElementById("searchForm").reset(); //after get music by artist returns what are you doing with the objects?
   }
 
   //Start coding here
@@ -15,28 +15,42 @@ function ItunesController() {
       const song = results[i];
       if (song.kind == 'song') {
         template += `
-        <div class="col-3">
-            <a onclick="app.controllers.itunesCtrl.pauseAll(${[i]})"><h2>Title: ${song.title}</h2></a>
-            <h3>Artist: ${song.artist}</h4>
-            <img src="${song.albumArt}" alt="">
-            <h4>Collection: ${song.collection}</h4>
-            <h4>Price: ${song.price}</h4>
-            <audio id="audio${[i]}" controls>
+        <div class="card col-md-3 col-xs-12 songs-box">
+            <a onclick="app.controllers.itunesCtrl.pauseAll(${[i]})"><h4>${song.title}</h4></a>
+            <h5>${song.artist}</h5>
+            <div class="img-container" style="background-image: url(${song.albumArt});">
+              <a onclick="app.controllers.itunesCtrl.pauseAll(${[i]})"><img class="play-img" src="http://simpleicon.com/wp-content/uploads/play1.png"></a>
+            </div>
+            <p class="mt-2">Collection: ${song.collection}</p>
+            <h6>Price: $${song.price}</h6>
+            <audio id="audio${[i]}" class="audio-player" controls>
               <source src="${song.preview}" type="audio/mp3">
             </audio>
+            <a href="${song.trackUrl}" target="_blank" class="btn btn-outline-success mt-2">Purchase</a>
         </div>`
       }
     }
     document.getElementById('songs-list').innerHTML = template
   }
 
+var playing = 0;
+
 //public
   this.pauseAll = function pauseAll(num) {
     var sounds = document.getElementsByTagName('audio');
     for (var i = 0; i < sounds.length; i++) {
-      sounds[i].pause()
-    }
-    document.getElementById("audio" + num).play();
+      if (sounds[i] == sounds[num]) {
+        if (playing == num) {
+          sounds[i].pause()
+          playing = -1
+        } else {
+          sounds[i].play()
+          playing = num
+        }
+      } else {
+        sounds[i].pause()
+      }
+    } 
   }
 }
 
--- itunes-service.js
+++ itunes-service.js
@@ -1,12 +1,17 @@
 function ItunesService(){
 
+  function swapUrlSize(url, pixels) {
+    var sizeString = `${pixels}x${pixels}`;
+    var newURL = url.replace("60x60", sizeString);
+    return newURL;
+  }
     this.getMusicByArtist = function(artist) {
 
       //allows requests to localhost: 8080 otherwise blocked by itunes
       var url = 'https://itunes.apple.com/search?term=' + artist;
       
       //changes the button to loading while songs load
-      $('#get-music-button').text('SEARCHING....');
+      $('#get-music-button').text('SEARCHING...');
       
       //modifies the objects to reduce the excess data
       return $.getJSON(url).then(function(response){
@@ -14,11 +19,13 @@ function ItunesService(){
                   return {
                       kind: song.kind,
                       title: song.trackName,
-                      albumArt: song.artworkUrl60,
+                      albumArt: swapUrlSize(song.artworkUrl60, 200),
                       artist: song.artistName,
                       collection: song.collectionName,
                       price: song.collectionPrice,
-                      preview: song.previewUrl
+                      preview: song.previewUrl,
+                      trackUrl: song.trackViewUrl,
+                      artistId: song.artistId
                     };
                 })
         //changes button back to GET MUSIC once songs are loaded
