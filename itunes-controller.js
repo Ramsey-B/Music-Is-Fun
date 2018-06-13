f70df6d11f5acce4a305afa6e93fe9a9333f9a00
Ramsey Bland <ramsey.bland@gmail.com>
2018-06-13 07:46:16 -0600

    added change image

--- itunes-controller.js
+++ itunes-controller.js
@@ -19,7 +19,7 @@ function ItunesController() {
             <a onclick="app.controllers.itunesCtrl.pauseAll(${[i]})"><h4>${song.title}</h4></a>
             <h5>${song.artist}</h5>
             <div class="img-container" id="imgBox${[i]}" style="background-image: url(${song.albumArt});">
-              <a onclick="app.controllers.itunesCtrl.play(${[i]})"><img class="play-img" src="pics/play.png"></a>
+              <a onclick="app.controllers.itunesCtrl.playSong(${[i]})"><img class="play-img" src="pics/play.png"></a>
             </div>
             <p class="mt-2">Collection: ${song.collection}</p>
             <h6>Price: $${song.price}</h6>
@@ -36,26 +36,24 @@ function ItunesController() {
   var playing = 0;
 
   //public
-  this.play = function(i) {
+  this.playSong = function(i) {
     document.getElementById('audio' +i).play()
-    document.getElementById('imgBox' +i).innerHTML = `<a onclick="app.controllers.itunesCtrl.pause(${i})"><img class="play-img" src="pics/pause.png"></a>`
+    document.getElementById('imgBox' +i).innerHTML = `<a onclick="app.controllers.itunesCtrl.pauseSong(${i})"><img class="play-img" src="pics/pause.png"></a>`
   }
 
-  this.pause = function(i) {
+  this.pauseSong = function(i) {
     document.getElementById('audio' +i).pause()
-    document.getElementById('imgBox' +i).innerHTML = `<a onclick="app.controllers.itunesCtrl.play(${i})"><img class="play-img" src="pics/play.png"></a>`
+    document.getElementById('imgBox' +i).innerHTML = `<a onclick="app.controllers.itunesCtrl.playSong(${i})"><img class="play-img" src="pics/play.png"></a>`
   }
   
   document.addEventListener('play', function (e) {
     var audios = document.getElementsByTagName('audio');
     for (var i = 0; i < audios.length; i++) {
       if (audios[i] != e.target) {
-        audios[i].pause();
-        audios[i].innerHTML = `<a onclick="app.controllers.itunesCtrl.play(${i})"><img class="play-img" src="pics/play.png"></a>`
+        app.controllers.itunesCtrl.pauseSong([i])
       }
     }
   }, true);
 }
-
 
 
